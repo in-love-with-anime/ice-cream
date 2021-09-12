c90ee8faba5d91c5f65b72f62a0d17bb0f4c1e9a
Dmytro Kalistratov <wobupeld@gmail.com>
2021-09-12 23:06:42 +0300

    Something good
    Hover/Focus. optimization .png

--- src/js/menu.js
+++ src/js/menu.js
@@ -36,3 +36,22 @@
   });
 
 })();
+
+// (() => {
+//     const refs = {
+//       modal: document.querySelector('[data-modal]'),
+//       openModalBtn: document.querySelector('[data-modal-open]'),
+//       closeModalBtn: document.querySelector('[data-modal-close]'),
+//       mobileMenu: document.querySelector('[data-menu-links]'),
+//     };
+// ​
+//     function toggleModal() {
+//         refs.modal.classList.toggle('is-open')
+//         refs.modal.classList.toggle('is-hidden');
+//     }
+// ​
+//     refs.openModalBtn.addEventListener('click', toggleModal);
+//     refs.closeModalBtn.addEventListener('click', toggleModal);
+// ​
+//     refs.mobileMenu.addEventListener('click', toggleModal);
+// })();
