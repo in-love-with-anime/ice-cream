163e0aac8ec91a74530f37829d62fbfc54ea4a5b
in-love-with-anime <vall.bell91@gmail.com>
2021-09-13 19:02:28 +0300

    ))

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,57 +1,16 @@
-// (() => {
-//     const menuBtnRef = document.querySelector("[data-menu-button]");
-//     const mobileMenuRef = document.querySelector("[data-menu]");
-
-//     menuBtnRef.addEventListener("click", () => {
-//         const expanded =
-//             menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-//         menuBtnRef.classList.toggle("is-open");
-//         menuBtnRef.setAttribute("aria-expanded", !expanded);
-        
-//         mobileMenuRef.classList.toggle("is-open");
-//     });
-// })()
-
+ 
 (() => {
-  const mobileMenu = document.querySelector('.mobile-nav');
-  const openMenuBtn = document.querySelector('.open-menu');
-  const closeMenuBtn = document.querySelector('.close-menu');
+    const el = query => document.querySelector(query);
+    const onclick = (elem, callback) => elem.addEventListener('click', callback)
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-  };
+    const toggleModal = () => {
+        const modal = el('[data-modal]')
+        modal.classList.toggle('is-open')
+        modal.classList.toggle('is-hidden')
+    };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-  });
+    onclick(el('[data-modal-open]'), toggleModal)
+    onclick(el('[data-modal-close]'), toggleModal)
+    onclick(el('[data-menu-mobile]'), toggleModal)
 
 })();
-
-// (() => {
-//     const refs = {
-//       modal: document.querySelector('[data-modal]'),
-//       openModalBtn: document.querySelector('[data-modal-open]'),
-//       closeModalBtn: document.querySelector('[data-modal-close]'),
-//       mobileMenu: document.querySelector('[data-menu-links]'),
-//     };
-// ​
-//     function toggleModal() {
-//         refs.modal.classList.toggle('is-open')
-//         refs.modal.classList.toggle('is-hidden');
-//     }
-// ​
-//     refs.openModalBtn.addEventListener('click', toggleModal);
-//     refs.closeModalBtn.addEventListener('click', toggleModal);
-// ​
-//     refs.mobileMenu.addEventListener('click', toggleModal);
-// })();
