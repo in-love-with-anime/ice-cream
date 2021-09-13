c9402ce0a452aaceb5a0d2d700070c58b88f847f
in-love-with-anime <vall.bell91@gmail.com>
2021-09-13 19:32:50 +0300

    )

--- src/js/header_buy-now.js
+++ src/js/header_buy-now.js
@@ -1,15 +1,17 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[header-modal-open]'),
-      closeModalBtn: document.querySelector('[header-modal-close]'),
-      modal: document.querySelector('[header-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      document.body.classList.toggle('modal-open');
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+  const modal = document.querySelector('[header-modal]');
+  const closeModalBtn = document.querySelector('[header-modal-close]');
+
+  const openModalBtns = document.querySelectorAll('[header-modal-open]');
+  const openModalBtn1 = openModalBtns[0];
+  const openModalBtn2 = openModalBtns[1];
+
+  openModalBtn1.addEventListener('click', toggleModal);
+  openModalBtn2.addEventListener('click', toggleModal);
+  closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    modal.classList.toggle('is-hidden');
+  }
+})();
